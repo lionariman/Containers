90c86118f577a4bfbba22a0bb2758b9b854f21fe
lionariman <lionariman@gmail.com>
2021-07-12 13:28:42 +0300

    start using allocators

--- vector/main.cpp
+++ vector/main.cpp
@@ -2,185 +2,67 @@
 
 #include <iostream>
 #include <vector>
+#include <memory>
 
-template<typename T>
-class Vector
+namespace ft
 {
-public:
-    typedef T typeValue;
-
-private:
-    T *_arr;
-    size_t _sz;
-    size_t _cap;
-
-public:
-
-    // ------------------------------------------
-    // Vector() : _arr(nullptr), _sz(0), _cap(0) {}
-
-    // virtual ~Vector() {}
-
-    // Vector &operator = (const Vector &other)
-    // {
-    //     if (this == &other)
-    //         return *this;
-    //     this->_arr = other._arr;
-    //     this->_sz = other._sz;
-    //     this->_cap = other._cap;
-    //     return *this;
-    // }
-
-    Vector() : _sz(0), _cap(0) { this->_arr = new T[0]; }
-
-    virtual ~Vector() { delete[] this->_arr; }
-
-    Vector(Vector const &other) { *this = other; }
-
-    Vector &operator = (Vector const &other)
+    template<
+        typename T,
+        typename Allocator = std::allocator<T>
+    > class vector
     {
-        if (this == &other)
+    public:
+        typedef T typeValue;
+
+    private:
+        T *_arr;
+        Allocator _alloc;
+        size_t _sz;
+        size_t _cap;
+
+    public:
+        vector(Allocator const &al = Allocator()) : _arr(0), _alloc(al), _sz(0), _cap(0) {}
+        ~vector() {}
+        vector(vector const &other) { *this = other; }
+        vector &operator = (vector const &other)
+        {
+            if (this == &other)
+                return *this;
+            this->_arr = other._arr;
+            this->_alloc = other._alloc;
+            this->_sz = other._sz;
+            this->_cap = other._cap;
             return *this;
-        delete[] this->_arr;
-        this->_arr = other._arr;
-        this->_sz = other._sz;
-        this->_cap = other._cap;
-        return *this;
-    }
-    // ------------------------------------------
-
-    size_t size() const { return this->_sz; }
-    size_t capacity() const { return this->_cap; }
-
-    void assign(size_t count, typeValue const &value = typeValue())
-    {
-        this->_sz = count;
-        this->_cap = count * 2;
-        delete[] this->_arr;
-        this->_arr = new typeValue[this->_cap];
-        for (size_t i(0); i < count; i++)
-            this->_arr[i] = value;
-    }
-
-    typeValue &operator [] (size_t const count) { return this->_arr[count]; }
-
-    typeValue &at(size_t const count) const
-    {
-        if (count < this->_sz)
-            return this->_arr[count];
-        throw std::runtime_error("wrong indexing");
-    }
-
-    void resize(size_t count, typeValue const &value = typeValue())
-    {
-        this->_cap = count * 2;
-        typeValue *newArr = new typeValue[this->_cap];
-        if (count < this->_sz)
-        {
-            for (size_t i(0); i < count; i++)
-                newArr[i] = this->_arr[i];
         }
-        else
+
+        void reserve(size_t n)
         {
-            for (size_t i(0); i < this->_sz; i++)
-                newArr[i] = this->_arr[i];
-            for (size_t i(this->_sz); i < count; i++)
-                newArr[i] = value;
+            this->_cap = n;
+            this->_arr = this->_alloc.allocate(this->_cap);
+            std::cout << ">> array has been alocated by allocator <<" << std::endl;
+            this->_alloc.deallocate(this->_arr, this->_cap);
+            std::cout << ">> array has been dealocated by allocator <<" << std::endl;
         }
-        delete[] this->_arr;
-        this->_arr = newArr;
-        this->_sz = count;
-    }
 
-    void reserve(size_t newCap)
-    {
-        if (newCap < this->_cap) return;
-        typeValue *newArr = new typeValue[newCap];
-        for (size_t i(0); i < this->_sz; i++)
-            newArr[i] = this->_arr[i];
-        delete[] this->_arr;
-        this->_arr = newArr;
-        this->_cap = newCap;
-    }
-
-    void push_back(const T &value)
-    {
-        if (value == NULL) return;
-        if (this->_sz == this->_cap)
-            reserve(this->_cap * 2);
-        this->_arr[this->_sz] = value;
-        this->_sz++;
-    }
-
-    // ----------------- iterators -----------------
-
-    class iterator
-    {
-        private:
-            typeValue *_ptr;
-        
-        public:
-            iterator() : _ptr(NULL) {}
-            ~iterator() { this->_ptr = NULL; }
-            iterator(iterator const &other) { *this = other; }
-            iterator(typeValue *value) { this->_ptr = value; }
-
-            iterator &operator = (iterator const &other) { this->_ptr = other._ptr; return *this; }
-            iterator &operator ++ () { return iterator(++this->_ptr); }
-            iterator &operator -- () { return iterator(--this->_ptr); }
-            bool operator == (iterator const &other) { return this->_ptr == other._ptr; }
-            bool operator <= (iterator const &other) { return this->_ptr <= other._ptr; }
-            bool operator >= (iterator const &other) { return this->_ptr >= other._ptr; }
-            bool operator > (iterator const &other) { return this->_ptr > other._ptr; }
-            bool operator < (iterator const &other) { return this->_ptr < other._ptr; }
-            bool operator != (iterator const &other) { return this->_ptr != other._ptr; }
-            typeValue &operator * () { return *this->_ptr; }
     };
+}
 
-    iterator begin() { return iterator(&this->_arr[0]); }
-    iterator end() { return iterator(&this->_arr[this->_sz + 1]); }
-
-};
 
 void foo(void)
 {
     try
     {
-        // std::vector<int> vec;
+        std::cout << "<====================>" << std::endl;
+        ft::vector<std::string> box;
+        ft::vector<std::string> box2;
 
-        // vec.push_back(1);
-        // vec.push_back(1);
-        // vec.push_back(1);
-        // vec.push_back(1);
-        // vec.push_back(1);
-        // vec.push_back(1);
+        box.reserve(5);
 
-        // std::cout << vec.at(0) << std::endl;
+        box2 = box;
 
-        // vec.clear();
+        box2.reserve(3);
 
-        // my vector
-        Vector<std::string> box;
-        Vector<std::string>::iterator iter;
-
-        box.assign(3, "zzz...");
-
-        box.resize(4, "> x <");
-        box.reserve(10);
-
-        iter = box.begin();
-        iter++;
-        iter++;
-        iter++;
-        std::cout << (iter != box.end()) << std::endl;
-        // while (iter != box.end())
-        //     std::cout << *iter << std::endl;
-
-        // box.push_back("world");
-        // std::cout << box.at(3) << std::endl;
-        // for (size_t i(0); i < box.size(); ++i)
-        //     std::cout << box[i] << std::endl;
-        std::cout << box.size() << ' ' << box.capacity() << std::endl;
+        std::cout << "<====================>" << std::endl;
 
     }
     catch(const std::exception& e) { std::cerr << e.what() << '\n'; }
--- vector/test_main.cpp
+++ vector/test_main.cpp
@@ -2,21 +2,5 @@
 
 int main(void)
 {
-    unsigned int buf1[sizeof(int) * 2];
-    unsigned int buf2[2];
-
-
-    // write items to buf1 and print them out
-    for (int i(0); i < 2; ++i)
-    {
-        buf1[i] = i;
-        std::cout << buf1[i] << std::endl;
-    }
-
-    // write items to buf2 and print them out
-    for (int i(0); i < 2; ++i)
-    {
-        buf2[i] = i;
-        std::cout << buf2[i] << std::endl;
-    }
+    std::vector<int> vec;
 }
