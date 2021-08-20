24c9a96ffbceb9eb7b52d68091369259aa645708
lionariman <lionariman@gmail.com>
2021-08-20 09:18:13 +0300

    assign operator (fill) is ready

--- vector.hpp
+++ vector.hpp
@@ -140,25 +140,26 @@ namespace ft
     class vector
     {
     public:
-        typedef size_t size_type;
+        typedef typename std::size_t size_type;
+        typedef typename std::ptrdiff_t difference_type;
         typedef T value_type;
         typedef Allocator allocator_type;
         typedef typename allocator_type::reference reference;
         typedef typename allocator_type::const_reference const_reference;
         typedef typename allocator_type::pointer pointer;
         typedef typename allocator_type::const_pointer const_pointer;
-        typedef vector_iterator<value_type> iterator;
-        typedef vector_iterator<const value_type> const_iterator;
-        typedef vector_reverse_iterator<value_type> reverse_iterator;
-        typedef vector_reverse_iterator<const value_type> const_reverse_iterator;
-        typedef std::ptrdiff_t difference_type;
+        typedef ft::vector_iterator<value_type> iterator;
+        typedef ft::vector_iterator<const value_type> const_iterator;
+        typedef ft::vector_reverse_iterator<value_type> reverse_iterator;
+        typedef ft::vector_reverse_iterator<const value_type> const_reverse_iterator;
         
 
     private:
-        template < class It >
-        difference_type distance(It first, It last)
+        difference_type distance(iterator first, iterator last)
         {
             difference_type i = 0;
+            if (*first == 0 && *last == 0)
+                return 0;
             for (; first != last; first++, i++);
             return i;
         }
@@ -376,7 +377,7 @@ namespace ft
 
         iterator insert(iterator position, value_type const &val)
         {
-            if (this->_size > this->_capacity)
+            if (this->_size + 1 > this->_capacity)
             {
                 value_type *tmp = this->_alloc.allocate(this->_capacity * 2);
                 for (size_type i(0); i < this->_size; i++)
@@ -388,7 +389,7 @@ namespace ft
                 this->_box = tmp;
                 this->_capacity *= 2;
             }
-            difference_type p = distance(begin(), position);
+            difference_type p = position - begin();
             for (size_type i = this->_size - 1; i != static_cast<size_type>(p) - 1; i--)
             {
                 this->_alloc.construct(this->_box + i + 1, *(this->_box + i));
@@ -401,10 +402,30 @@ namespace ft
 
         // fill
 
-        // void insert(iterator position, size_type n, value_type const &val)
-        // {
-
-        // }
+        void insert(iterator position, size_type n, value_type const &val)
+        {
+            if (this->_size + n > this->_capacity)
+            {
+                value_type *tmp = this->_alloc.allocate(this->_size + n);
+                for (size_type i(0); i < this->_size; i++)
+                {
+                    this->_alloc.construct(tmp + i, *(this->_box + i));
+                    this->_alloc.destroy(this->_box + i);
+                }
+                this->_alloc.deallocate(this->_box, this->_capacity);
+                this->_box = tmp;
+                this->_capacity = this->_size + n;
+            }
+            difference_type p = distance(begin(), position);
+            for (size_type i = this->_size - 1; i != static_cast<size_type>(p) - 1; i--)
+            {
+                this->_alloc.construct(this->_box + i + n, *(this->_box + i));
+                this->_alloc.destroy(this->_box + i);
+            }
+            for (size_type i = static_cast<size_type>(p); i < static_cast<value_type>(p) + n; i++)
+                this->_alloc.construct(this->_box + i, val);
+            this->_size += n;
+        }
 
         // range
 
