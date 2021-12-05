a24142effe7e1964e05136eb251c6e428cec07bd
lionariman <lionariman@gmail.com>
2021-12-05 19:30:42 +0300

    ...

--- main.cpp
+++ main.cpp
@@ -12,256 +12,259 @@
 #define MY_ALLOCATOR 0
 #define STATE 0
 
-// template < typename T >
-// void push_back_numbers(T &numbers, int numSize = int())
-// {
-//     // std::cout << GREEN << "[ ... Push back numbers (" << numSize << ") ... ]\n" << NCLR;
-//     for (int i(0); i < numSize; i++)
-//         numbers.push_back(i);
-// }
+template < typename T >
+void push_back_numbers(T &numbers, int numSize = int())
+{
+    // std::cout << GREEN << "[ ... Push back numbers (" << numSize << ") ... ]\n" << NCLR;
+    for (int i(0); i < numSize; i++)
+        numbers.push_back(i);
+}
 
-// template < typename T >
-// void pop_back_numbers(T &numbers, int times = int())
-// {
-//     std::cout << GREEN << "[ ... Pop back numbers (" << times << ") ... ]\n" << NCLR;
-//     for (int i(0); i < times; i++)
-//         numbers.pop_back();
-// }
+template < typename T >
+void pop_back_numbers(T &numbers, int times = int())
+{
+    std::cout << GREEN << "[ ... Pop back numbers (" << times << ") ... ]\n" << NCLR;
+    for (int i(0); i < times; i++)
+        numbers.pop_back();
+}
 
-// template < typename T1, typename T2 >
-// void print_each_element_and_size(T1 &std_numbers, T2 &my_numbers, int how = int())
-// {
-//     size_t i(0);
+template < typename T1, typename T2 >
+void print_each_element_and_size(T1 &std_numbers, T2 &my_numbers, int how = int())
+{
+    size_t i(0);
 
-//     // std::cout << YELLOW << "\n[ ... Print each element size ... ]\n\n" << NCLR;
+    // std::cout << YELLOW << "\n[ ... Print each element size ... ]\n\n" << NCLR;
 
-//     if (how)
-//     {
-//         for (; i < std_numbers.size(); i++)
-//             std::cout << std_numbers.at(i) << '\n';
-//         std::cout << "[" << i << "]\n";
-//         i = 0;
-//         for (; i < my_numbers.size(); i++)
-//             std::cout << my_numbers.at(i) << '\n';
-//         std::cout << "[" << i << "]\n";
-//         return ;
-//     }
-//     for (; i < std_numbers.size(); i++)
-//         std::cout << std_numbers.at(i) << ' ';
-//     std::cout << " [" << i << "] \n";
-//     i = 0;
-//     for (; i < my_numbers.size(); i++)
-//         std::cout << my_numbers.at(i) << ' ';
-//     std::cout << " [" << i << "] \n";
-// }
+    if (how)
+    {
+        for (; i < std_numbers.size(); i++)
+            std::cout << std_numbers.at(i) << '\n';
+        std::cout << "[" << i << "]\n";
+        i = 0;
+        for (; i < my_numbers.size(); i++)
+            std::cout << my_numbers.at(i) << '\n';
+        std::cout << "[" << i << "]\n";
+        return ;
+    }
+    for (; i < std_numbers.size(); i++)
+        std::cout << std_numbers.at(i) << ' ';
+    std::cout << " [" << i << "] \n";
+    i = 0;
+    for (; i < my_numbers.size(); i++)
+        std::cout << my_numbers.at(i) << ' ';
+    std::cout << " [" << i << "] \n";
+}
 
-// template < typename T1, typename T2 >
-// void print_size_and_capacity(T1 &std_numbers, T2 &my_numbers)
-// {
+template < typename T1, typename T2 >
+void print_size_and_capacity(T1 &std_numbers, T2 &my_numbers)
+{
 
-//     // std::cout << YELLOW << "\n[ ... Print size and capacity ... ]\n\n" << NCLR;
+    // std::cout << YELLOW << "\n[ ... Print size and capacity ... ]\n\n" << NCLR;
 
-//     std::cout << "    std size: " << std_numbers.size() << '\n'
-//               << "     my size: " << my_numbers.size() << '\n'
-//               << "std capacity: " << std_numbers.capacity() << '\n'
-//               << " my capacity: " << my_numbers.capacity() << "\n\n";
-// }
+    std::cout << "    std size: " << std_numbers.size() << '\n'
+              << "     my size: " << my_numbers.size() << '\n'
+              << "std capacity: " << std_numbers.capacity() << '\n'
+              << " my capacity: " << my_numbers.capacity() << "\n\n";
+}
 
-// void test_one()
-// {
-//         // std::cout << GREEN << ">> EMPTY VECTOR OF INTS <<\n" << NCLR;
+void test_one()
+{
+        // std::cout << GREEN << ">> EMPTY VECTOR OF INTS <<\n" << NCLR;
 
-// #if MY_ALLOCATOR
-//         std::cout << B_PURPLE << "... USING MY OWN ALLOCATOR ...\n" << NCLR;
-//         std::vector< int, ft::allocator<int> > std_numbers;
-//         ft::vector< int, ft::allocator<int> > my_numbers;
-// #else
-//         std::vector<int> std_numbers;
-//         ft::vector<int> my_numbers;
-// #endif
+#if MY_ALLOCATOR
+        std::cout << B_PURPLE << "... USING MY OWN ALLOCATOR ...\n" << NCLR;
+        std::vector< int, ft::allocator<int> > std_numbers;
+        ft::vector< int, ft::allocator<int> > my_numbers;
+#else
+        std::vector<int> std_numbers;
+        ft::vector<int> my_numbers;
+#endif
 
 
-// #if 0
-//         std::cout << BLUE << "<================ RESIZE ================>\n" << NCLR;
+#if 1
+        std::cout << BLUE << "<================ RESIZE ================>\n" << NCLR;
 
-//         std_numbers.resize(10, 1);
-//         my_numbers.resize(10, 1);
+        std_numbers.resize(10, 1);
+        my_numbers.resize(10, 1);
 
-//         print_each_element_and_size(std_numbers, my_numbers);
-//         print_size_and_capacity(std_numbers, my_numbers);
+        print_each_element_and_size(std_numbers, my_numbers);
+        print_size_and_capacity(std_numbers, my_numbers);
 
-//         std::cout << BLUE << "<================ RESERVE ================>\n" << NCLR;
+        std::cout << BLUE << "<================ RESERVE ================>\n" << NCLR;
 
-//         std_numbers.reserve(16);
-//         my_numbers.reserve(16);
+        std_numbers.reserve(16);
+        my_numbers.reserve(16);
 
-//         print_each_element_and_size(std_numbers, my_numbers);
-//         print_size_and_capacity(std_numbers, my_numbers);
+        print_each_element_and_size(std_numbers, my_numbers);
+        print_size_and_capacity(std_numbers, my_numbers);
 
-//         std::cout << BLUE << "<================ ITERATOR ================>\n" << NCLR;
+        std::cout << BLUE << "<================ ITERATOR ================>\n" << NCLR;
 
-//         push_back_numbers(std_numbers, 10);
-//         push_back_numbers(my_numbers, 10);
+        push_back_numbers(std_numbers, 10);
+        push_back_numbers(my_numbers, 10);
 
-//         std::vector<int>::iterator stdIterator = std_numbers.begin();
-//         ft::vector<int>::iterator myIterator = my_numbers.begin();
+        std::vector<int>::iterator stdIterator = std_numbers.begin();
+        ft::vector<int>::iterator myIterator = my_numbers.begin();
 
-//         std::cout << YELLOW << "[ std numbers ]\n" << NCLR;
-//         for (stdIterator = std_numbers.begin(); stdIterator != std_numbers.end(); stdIterator++)
-//             std::cout << *stdIterator << ' ';
+        std::cout << YELLOW << "[ std numbers ]\n" << NCLR;
+        for (stdIterator = std_numbers.begin(); stdIterator != std_numbers.end(); stdIterator++)
+            std::cout << *stdIterator << ' ';
 
-//         nl;
+        nl;
 
-//         std::cout << YELLOW << "[ my numbers ]\n" << NCLR;
-//         for (myIterator = my_numbers.begin(); myIterator != my_numbers.end(); myIterator++)
-//             std::cout << *myIterator << ' ';
+        std::cout << YELLOW << "[ my numbers ]\n" << NCLR;
+        for (myIterator = my_numbers.begin(); myIterator != my_numbers.end(); myIterator++)
+            std::cout << *myIterator << ' ';
 
-//         std::cout << "\n\n";
-// #endif
+        std::cout << "\n\n";
+#endif
 
-// #if 0
-//         std::cout << BLUE << "<========== ASSIGN ==========>\n" << NCLR;
+#if 1
+        std::cout << BLUE << "<========== ASSIGN ==========>\n" << NCLR;
 
-//         push_back_numbers(std_numbers, 17);
-//         push_back_numbers(my_numbers, 17);
-//         print_each_element_and_size(std_numbers, my_numbers);
-//         print_size_and_capacity(std_numbers, my_numbers);
+        push_back_numbers(std_numbers, 17);
+        push_back_numbers(my_numbers, 17);
+        print_each_element_and_size(std_numbers, my_numbers);
+        print_size_and_capacity(std_numbers, my_numbers);
 
-//         std::cout << PURPLE << "... current vertors ... \n" << NCLR;
-//         print_each_element_and_size(std_numbers, my_numbers);
-//         print_size_and_capacity(std_numbers, my_numbers);
+        std::cout << PURPLE << "... current vertors ... \n" << NCLR;
+        print_each_element_and_size(std_numbers, my_numbers);
+        print_size_and_capacity(std_numbers, my_numbers);
 
-//         std::vector<int> new_s;
-//         ft::vector<int> new_m;
+        std::vector<int> new_s;
+        ft::vector<int> new_m;
 
-//         std::cout << PURPLE << "... new vertors ... \n" << NCLR;
+        std::cout << PURPLE << "... new vertors ... \n" << NCLR;
 
-//         new_s.assign(std_numbers.crbegin(), std_numbers.crend());
-//         new_m.assign(my_numbers.crbegin(), my_numbers.crend());
+        new_s.assign(std_numbers.crbegin(), std_numbers.crend());
+        new_m.assign(my_numbers.crbegin(), my_numbers.crend());
 
-//         print_each_element_and_size(new_s, new_m);
-//         print_size_and_capacity(new_s, new_m);
-// #endif
+        print_each_element_and_size(new_s, new_m);
+        print_size_and_capacity(new_s, new_m);
+#endif
 
-// #if 0
-//         std::cout << BLUE << "<========== INSERT ==========>\n" << NCLR;
+#if 1
+        std::cout << BLUE << "<========== INSERT ==========>\n" << NCLR;
 
-//         push_back_numbers(std_numbers, 10);
-//         push_back_numbers(my_numbers, 10);
-//         print_each_element_and_size(std_numbers, my_numbers);
-//         print_size_and_capacity(std_numbers, my_numbers);
+        push_back_numbers(std_numbers, 10);
+        push_back_numbers(my_numbers, 10);
+        print_each_element_and_size(std_numbers, my_numbers);
+        print_size_and_capacity(std_numbers, my_numbers);
 
-//         std::cout << PURPLE << " ... after insert ... \n" << NCLR;
+        std::cout << PURPLE << " ... after insert ... \n" << NCLR;
 
-//         //                 [    position    ]   [        first         ]  [         last          ]
-//         std_numbers.insert(std_numbers.begin(), std_numbers.begin() + 2, std_numbers.begin() + 8);
-//         my_numbers.insert(my_numbers.begin(), my_numbers.begin() + 2, my_numbers.begin() + 8);
+        //                 [    position    ]   [        first         ]  [         last          ]
+        std_numbers.insert(std_numbers.begin(), std_numbers.begin() + 2, std_numbers.begin() + 8);
+        my_numbers.insert(my_numbers.begin(), my_numbers.begin() + 2, my_numbers.begin() + 8);
         
-//         print_each_element_and_size(std_numbers, my_numbers);
-//         print_size_and_capacity(std_numbers, my_numbers);
+        print_each_element_and_size(std_numbers, my_numbers);
+        print_size_and_capacity(std_numbers, my_numbers);
 
-// #endif
+#endif
 
-// #if 0
-//         std::cout << BLUE << "<=========== ERASE ==========>\n" << NCLR;
-//         push_back_numbers(std_numbers, 11);
-//         push_back_numbers(my_numbers, 11);
-//         print_each_element_and_size(std_numbers, my_numbers);
-//         print_size_and_capacity(std_numbers, my_numbers);
+#if 1
+        std::cout << BLUE << "<=========== ERASE ==========>\n" << NCLR;
+        push_back_numbers(std_numbers, 11);
+        push_back_numbers(my_numbers, 11);
+        print_each_element_and_size(std_numbers, my_numbers);
+        print_size_and_capacity(std_numbers, my_numbers);
 
-//         // std_numbers.erase(std_numbers.begin(), std_numbers.begin() + 5);
-//         // my_numbers.erase(my_numbers.begin(), my_numbers.begin() + 5);
+        // std_numbers.erase(std_numbers.begin(), std_numbers.begin() + 5);
+        // my_numbers.erase(my_numbers.begin(), my_numbers.begin() + 5);
 
-//         // print_each_element_and_size(std_numbers, my_numbers);
-//         // print_size_and_capacity(std_numbers, my_numbers);
+        // print_each_element_and_size(std_numbers, my_numbers);
+        // print_size_and_capacity(std_numbers, my_numbers);
 
-//         std::cout << " ... vector comparison ...\n";
+        std::cout << " ... vector comparison ...\n";
 
-//         ft::vector<int> zzz;
-//         std::vector<int> xxx;
-//         push_back_numbers(zzz, 11);
-//         push_back_numbers(xxx, 11);
+        ft::vector<int> zzz;
+        std::vector<int> xxx;
+        push_back_numbers(zzz, 11);
+        push_back_numbers(xxx, 11);
 
-//         print_each_element_and_size(xxx, zzz);
-//         print_size_and_capacity(xxx, zzz);
+        print_each_element_and_size(xxx, zzz);
+        print_size_and_capacity(xxx, zzz);
 
-//         std::cout << "zzz > my_numbers returns " << (zzz > zzz) << '\n';
-//         std::cout << "xxx > std_numbers returns " << (xxx > xxx) << '\n';
-// #endif
-// }
+        std::cout << "zzz > my_numbers returns " << (zzz == zzz) << '\n';
+        std::cout << "xxx > std_numbers returns " << (xxx == xxx) << '\n';
 
-// void test_two()
-// {
-// #if MY_ALLOCATOR
-//         std::cout << B_PURPLE << "... USING MY OWN ALLOCATOR ...\n" << NCLR;
+#endif
+}
 
-//         std::vector< std::string, ft::allocator<std::string> > std_strings;
+void test_two()
+{
+#if MY_ALLOCATOR
+        std::cout << B_PURPLE << "... USING MY OWN ALLOCATOR ...\n" << NCLR;
 
-//         ft::vector< std::string, ft::allocator<std::string> > my_strings;
-//         ft::vector< std::string, ft::allocator<std::string> > my_strings2;
-// #else
-//         std::vector<std::string> std_strings;
-//         ft::vector<std::string> my_strings;
-//         ft::vector<std::string> my_strings2;
-// #endif
+        std::vector< std::string, ft::allocator<std::string> > std_strings;
 
-//     for (size_t i(0); i < 10; i++)
-//         std_strings.push_back("x");
+        ft::vector< std::string, ft::allocator<std::string> > my_strings;
+        ft::vector< std::string, ft::allocator<std::string> > my_strings2;
+#else
+        std::vector<std::string> std_strings;
+        ft::vector<std::string> my_strings;
+        ft::vector<std::string> my_strings2;
+#endif
 
-//     for (size_t i(0); i < 10; i++)
-//         my_strings.push_back("x");
+    for (size_t i(0); i < 10; i++)
+        std_strings.push_back("x");
 
-//     for (size_t i(0); i < 10; i++)
-//         my_strings2.push_back("x");
+    for (size_t i(0); i < 10; i++)
+        my_strings.push_back("x");
 
-//     print_each_element_and_size(my_strings, my_strings2);
-//     print_size_and_capacity(my_strings, my_strings2);
+    for (size_t i(0); i < 10; i++)
+        my_strings2.push_back("x");
 
-//     my_strings = my_strings2;
+    print_each_element_and_size(my_strings, my_strings2);
+    print_size_and_capacity(my_strings, my_strings2);
 
-//     print_each_element_and_size(my_strings, my_strings2);
-//     print_size_and_capacity(my_strings, my_strings2);
-// }
+    my_strings = my_strings2;
 
-// void test_three()
-// {
-//         std::cout << BLUE << "<=========== STACK ==========>\n" << NCLR;
+    print_each_element_and_size(my_strings, my_strings2);
+    print_size_and_capacity(my_strings, my_strings2);
+}
 
-//         std::stack<int> sst;
-//         ft::stack<int> mst;
-//         ft::stack<int> xst;
+void test_three()
+{
+        std::cout << BLUE << "<=========== STACK ==========>\n" << NCLR;
 
-//         sst.push(22);
-//         sst.push(22);
-//         sst.push(55);
-//         sst.push(77);
 
-//         mst.push(33);
-//         mst.push(33);
-//         mst.push(33);
-//         mst.push(44);
+        std::stack<int> sst;
+        ft::stack<int> mst;
+        std::cout << "\n#############\n";
+        ft::stack<int> xst;
 
-//         xst.push(33);
-//         xst.push(33);
-//         xst.push(33);
-//         xst.push(44);
+        sst.push(22);
+        sst.push(22);
+        sst.push(55);
+        sst.push(77);
 
-//         std::cout << "________sst________\n";
-//         std::cout << "TOP: " << sst.top() << '\n';
-//         std::cout << "________mst________\n";
-//         std::cout << "TOP: " << mst.top() << '\n';
+        mst.push(33);
+        mst.push(33);
+        mst.push(33);
+        mst.push(44);
 
-//         sst.pop();
-//         mst.pop();
-//         xst.pop();
+        xst.push(33);
+        xst.push(33);
+        xst.push(33);
+        xst.push(44);
 
-//         std::cout << "________sst________\n";
-//         std::cout << "TOP: " << sst.top() << '\n';
-//         std::cout << "________mst________\n";
-//         std::cout << "TOP: " << mst.top() << '\n';
+        std::cout << "________sst________\n";
+        std::cout << "TOP: " << sst.top() << '\n';
+        std::cout << "________mst________\n";
+        std::cout << "TOP: " << mst.top() << '\n';
 
-//         std::cout << "mst == xst returns " << (mst == xst) << '\n';
-// }
+        sst.pop();
+        mst.pop();
+        xst.pop();
+
+        std::cout << "________sst________\n";
+        std::cout << "TOP: " << sst.top() << '\n';
+        std::cout << "________mst________\n";
+        std::cout << "TOP: " << mst.top() << '\n';
+
+        std::cout << "mst == xst returns " << (mst == xst) << '\n';
+}
 
 void test_four()
 {
@@ -608,6 +611,25 @@ void test_four()
     std::map<int, int> new_smap(smap);
     ft::map<int, int> new_zmap(zmap);
 
+    // it = smap.begin();
+    // zit = zmap.begin();
+
+    ft::map<int, int>::iterator new_iter = new_zmap.begin();
+
+    // std::cout << "s____________________________\n\n";
+    // while (it != smap.end())
+    // {
+    //     std::cout << it->first << ' ';
+    //     it++;
+    // }
+    nl;
+    std::cout << "z____________________________\n\n";
+    while (new_iter != new_zmap.end())
+    {
+        std::cout << new_iter->first << ' ';
+        new_iter++;
+    }
+
     // zmap.callInOrder();
 
     // while (1);
@@ -622,8 +644,8 @@ int main()
         // std::cout << B_BLUE << "\n################# START #################\n\n" << NCLR;
         // test_one();
         // test_two();
-        // test_three();
-        test_four();
+        test_three();
+        // test_four();
     }
     catch(std::exception const &e)
     {
